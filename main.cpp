ebc78135ee708f979814d04c54d2839eb4cbd41e
FrantisekMekuta <113998165+FrantisekMekuta@users.noreply.github.com>
2022-09-27 15:43:52 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    std::cout <<"Ahoj\n";
+
+    int a = 15;
+    int d = 7;
+
+    float b = 45.5;
+
+    float c = std::sqrt(b);
+
+    std::cout <<"Cislo c = " << a / float(d) << "\n";
+
+    return 0;
+    }
